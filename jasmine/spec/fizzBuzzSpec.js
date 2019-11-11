3f0773febaedb2b4a581a23950810e64045bf04c
Robert Woolley <rob@robertwoolley.co.uk>
2019-11-11 16:50:22 +0000

    Fizzbuzz completed with tests in Javascript

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -5,4 +5,13 @@ describe ("fizzBuzz", function(){
   it("returns 1 when 1 is passed", function(){
     expect(fizzBuzz(1)).toEqual(1);
   });
+  it("returns fizz when 3 is passed", function(){
+    expect(fizzBuzz(3)).toEqual("fizz");
+  });
+  it("returns buzz when 5 is passed", function(){
+    expect(fizzBuzz(5)).toEqual("buzz");
+  });
+  it("returns fizzBuzz when 45 is passed", function(){
+    expect(fizzBuzz(45)).toEqual("fizzbuzz");
+  })
 });
--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -1,6 +1,10 @@
 function fizzBuzz(number) {
-  if (number === 15) {
+  if (number % 15 === 0) {
     return "fizzbuzz";
+  } else if  (number % 3 === 0){
+    return "fizz";
+  } else if (number % 5 === 0){
+    return "buzz";
   } else {
     return number;
   }
